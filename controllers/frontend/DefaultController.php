98cdda905008ff94f9ab9df4a5c1a007caae1a5d
ilknurgok <ilknurgok.1@hotmail.com>
2018-12-11 13:22:10 +0300

    initial commit

---
+++ controllers/frontend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\dersprogrami\controllers\frontend;
+
+
+/**
+ * Default controller for the `dersprogrami` module
+ */
+class DefaultController extends \kouosl\base\controllers\frontend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
